129182578794fea55924254df5928e4682fb78f8
Lsong <song940@gmail.com>
2020-10-28 23:26:03 -0400

    add server

---
+++ server.js
@@ -0,0 +1,48 @@
+const http = require('http');
+const crypto = require('crypto');
+
+const readStream = req => new Promise((resolve, reject) => {
+  var output = '';
+  req
+  .on('error', reject)
+  .on('data', c => output += c)
+  .on('end', () => resolve(output));
+});
+
+const dingtalk = ({ secret }, fn) => {
+  return async (req, res) => {
+    const { timestamp, sign, token } = req.headers;
+    const str = [ timestamp, secret ].join('\n');
+    const sha256 = crypto.createHmac('sha256', secret);
+    const hash = sha256.update(str).digest('base64');
+    console.log(hash, sign);
+
+    if(hash !== sign) return res.end('Invalid Signature');
+
+    const data = await readStream(req);
+    const body = JSON.parse(data);
+    const { msgtype } = body;
+    const message = await fn(body[msgtype].content, body);
+    res.end(JSON.stringify(message));
+  };
+};
+
+const reply = (msgtype, data) => {
+  return {
+     msgtype,
+     [msgtype]: data,
+     at: {
+         atMobiles: [], 
+         isAtAll: false
+     }
+   };
+};
+
+const app = dingtalk({ 
+  secret: '_QiOA0W298zwRWaXZ4ZjzjIh6HAcqcqWAzg5FfaO2mgojT1v96w-VfkbBK_rPL2Q'
+}, async (content, message) => {
+  console.log('message:', message); 
+  return reply('text', { content });
+});
+
+http.createServer(app).listen(3002);
